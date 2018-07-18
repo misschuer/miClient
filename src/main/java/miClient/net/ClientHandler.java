b08e01b0969d9f56ef37a512c5cdb2b46d40c8e7
gong <403575226@qq.com>
2018-07-18 14:32:06 +0800

    增加客户端示例的逻辑

--- src/main/java/miClient/net/ClientHandler.java
+++ src/main/java/miClient/net/ClientHandler.java
@@ -1,10 +1,33 @@
 package miClient.net;
 
 import cc.mi.core.handler.ChannelHandlerGenerator;
+import cc.mi.core.log.CustomLogger;
+import cc.mi.core.packet.Packet;
 import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
-public class ClientHandler extends ChannelInboundHandlerAdapter implements ChannelHandlerGenerator {
+public class ClientHandler extends SimpleChannelInboundHandler<Packet> implements ChannelHandlerGenerator {
+	static final CustomLogger logger = CustomLogger.getLogger(ClientHandler.class);
+	
+	public void channelActive(final ChannelHandlerContext ctx) {
+		
+	}
+	
+	@Override
+	public void channelRead0(final ChannelHandlerContext ctx, final Packet coder) throws Exception {
+		
+	}
+	
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		ctx.fireChannelInactive();
+	}
+
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable throwable) {
+		throwable.printStackTrace();
+		ctx.close();
+	}
+	
 	@Override
 	public ChannelHandler newChannelHandler() {
 		return new ClientHandler();
